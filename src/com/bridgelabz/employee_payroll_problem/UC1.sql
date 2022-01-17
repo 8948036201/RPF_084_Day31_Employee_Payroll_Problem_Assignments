96ce227d1870e3bc03a5259a2d20d721eb81a239
8948036201 <azharuddin.ansari108@gmail.com>
2022-01-17 17:23:10 +0530

    Ability to create a payroll

---
+++ src/com/bridgelabz/employee_payroll_problem/UC1.sql
@@ -0,0 +1,19 @@
+#UC1
+
+mysql> create database payroll_service;
+Query OK, 1 row affected (0.25 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sys                |
++--------------------+
+5 rows in set (0.04 sec)
+
+mysql> use payroll_service;
+Database changed
