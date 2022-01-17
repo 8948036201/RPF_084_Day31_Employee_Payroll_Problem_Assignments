78551f6279ae264cfaf48c549e326bcd64ac7ff0
8948036201 <azharuddin.ansari108@gmail.com>
2022-01-17 17:27:09 +0530

    Ability to create employee payroll data in the payroll service database as part of CRUD Operation

---
+++ bin/com/bridgelabz/employee_payroll_problem/UC3.sql
@@ -0,0 +1,11 @@
+#UC-3
+
+mysql> insert into employee_payroll (name,salary,start_date)
+    -> values ('Azhar',100000.00,'2021-10-29'),
+    -> ('Priyanka',90000.00,'2021-10-28'),
+    -> ('Khushbu',200000.00,'2022-01-22'),
+    -> ('Azharuddin',300000.00,'2022-03-01'),
+    -> ('Jack',50000.00,'2020-05-06')
+    -> ;
+Query OK, 5 rows affected (0.40 sec)
+Records: 5  Duplicates: 0  Warnings: 0
