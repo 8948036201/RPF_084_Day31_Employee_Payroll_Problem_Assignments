d3ff24e4061daf73fd560e1b8c6ae0156682f9e0
8948036201 <azharuddin.ansari108@gmail.com>
2022-01-18 10:05:10 +0530

    Ability to create a Address Book Service DB

---
+++ src/com/bridgelabz/address/book/system/UC1.sql
@@ -0,0 +1,17 @@
+#UC-1
+
+mysql> create database address_book_service;
+Query OK, 1 row affected (0.38 sec)
+
+mysql> show databases;
++----------------------+
+| Database             |
++----------------------+
+| address_book_service |
+| information_schema   |
+| mysql                |
+| payroll_service      |
+| performance_schema   |
+| sys                  |
++----------------------+
+6 rows in set (0.00 sec)
